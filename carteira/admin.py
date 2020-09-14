50c395969b3e87c9bda132a03ff3c7d2f759d22d
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-09-13 22:07:04 -0300

    comit

--- carteira/admin.py
+++ carteira/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Mes, Carteira
 
-# Register your models here.
+admin.site.register(Mes)
+admin.site.register(Carteira)
--- carteira/models.py
+++ carteira/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 class Mes(models.Model):
     mes = models.CharField(max_length=10)
+    def __str__(self):
+        return self.mes
+
 
 class Carteira(models.Model):
     mes = models.ForeignKey(Mes, on_delete=models.CASCADE)
     papel = models.CharField(max_length=10)
     quantidade = models.IntegerField()
     preco_medio = models.DecimalField(max_digits=20, decimal_places=2)
-    tipo_mercado = models.BooleanField()
+    dolarizado = models.BooleanField()
+
+    def __str__(self):
+        return self.papel
