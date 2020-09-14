d076a55043c7d5fd26be757aa0e1e3a7bf4cbb6e
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-09-13 22:41:16 -0300

    comit

--- carteira/admin.py
+++ carteira/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Mes, Carteira
 
-admin.site.register(Mes)
-admin.site.register(Carteira)
+class CarteiraAdmin(admin.ModelAdmin):
+    list_display = ('mes_carteira','papel','quantidade','preco_medio','dolarizado')
+admin.site.register(Carteira, CarteiraAdmin)
--- carteira/models.py
+++ carteira/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 
 class Mes(models.Model):
-    mes = models.CharField(max_length=10)
+    mes_do_ano = models.CharField(max_length=10)
     def __str__(self):
-        return self.mes
+        return self.mes_do_ano
 
 
 class Carteira(models.Model):
-    mes = models.ForeignKey(Mes, on_delete=models.CASCADE)
+    mes_carteira = models.ForeignKey(Mes, on_delete=models.CASCADE)
     papel = models.CharField(max_length=10)
     quantidade = models.IntegerField()
     preco_medio = models.DecimalField(max_digits=20, decimal_places=2)
     dolarizado = models.BooleanField()
-
     def __str__(self):
         return self.papel
